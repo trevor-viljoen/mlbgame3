9b16626830c263be241c782c9ecb1c8dc9138f6f
Trevor Viljoen <trevor.viljoen@gmail.com>
2018-02-23 17:06:25 -0600

    Add custom exceptions classes

---
+++ mlbgame/exceptions.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""MLBGame Exceptions"""
+
+class RequestException(Exception):
+    pass
+
+class ImplementationException(Exception):
+    pass
+
+class ObjectNotFoundException(Exception):
+    pass
+

ec71e044dafcff8e3c646fe76a7ca52c6164991c
Trevor Viljoen <trevor.viljoen@gmail.com>
2018-03-05 00:04:01 -0600

    Add documentation for schedule API endpoint

--- mlbgame/data/gameday.py
+++ mlbgame/data/gameday.py
@@ -11,8 +11,114 @@ endpoints.
 
 from datetime import datetime
 from mlbgame.data import request
+import mlbgame.exceptions
 
 TODAY = datetime.now().strftime("%m/%d/%Y")
 
 def get_schedule(params={'sportId': 1, 'date': TODAY}):
+    """
+    This endpoint allows you to pull shedules.
+    params:
+      calendarTypes
+        Description: Comma delimitd list of type of events
+        Parameter Type: query
+        Data Type: array[string]
+      teamId
+        Description: Unique Team Identifier.
+        Format: 141, 147, etc.
+        Parameter Type: query
+        Data Type: array[integer]
+      leagueId
+        Description: Unique League Identifier
+        Parameter Type: query
+        Data Type: array[integer]
+      sportId
+        Description: Top level organization of a sport
+        Format: SportId is 1 for MLB
+        Parameter Type: query
+        Data Type: array[integer]
+      gamePk
+        Description: Unique Primary Key representing a game
+        Parameter Type: query
+        Data Type: integer
+      gamePks
+        Description: Comma delimited list of unique primary keys
+        Parameter Type: query
+        Data Type: array[integer]
+      eventIds
+        Description: A unique identifier for non-game event
+        Parameter Type: query
+        Data Type: array[integer]
+      venueIds
+        Description: Unique Venue Identifier
+        Parameter Type: query
+        Data Type: array[integer]
+      performerIds
+        Description: A unique identifier for non-team event performers
+        Parameter Type: query
+        Data Type: array[integer]
+      gameType
+        Description: Type of Game. Available types in api/v1/gameTypes
+        Parameter Type: query
+        Data Type: array[string]
+      gameTypes
+        Description: Comma delimited list of type of Game.
+        Parameter Type: query
+        Data Type: array[string]
+      season
+        Description: Season of play
+        Parameter Type: query
+        Data Type: array[string]
+      seasons
+        Description: Comma delimited list of seasons of play
+        Parameter Type: query
+        Data Type: array[string]
+      date
+        Description: Date of Game
+        Format: MM/DD/YYYY
+        Parameter Type: query
+        Data Type: LocalDate
+      startDatem
+        Description: Start date for range of data. Must be used with end date.
+        Format: MM/DD/YYYY
+        Parameter Type: query
+        Data Type: LocalDate
+      endDate
+        Description: End date for range of data. Must be used with start date.
+        Format: MM/DD/YYYY
+        Parameter Type: query
+        Data Type: LocalDate
+      timecode
+        Description: Use this parameter to return a snapshot of the data at
+          the specified time.
+        Format: YYYYMMDD_HHMMSS
+        Parameter Type: query
+        Data Type: string
+      useLatestGames
+        Description: userLatestGames???
+        Parameter Type: query
+        Data Type: boolean
+      opponentId
+        Description: A unique identifier for the opposing team. Must be usted
+          with teamId.
+        Parameter Type: query
+        Data Type: integer (Documentation says array[integer])
+      fields
+        Description: Comma delimited list of specific fields to be returned.
+        Format: topLevelNode, childNode, attribute
+        Parameter Type: query
+        Data Type: array[string]
+    """
+    if 'startDate' in params.keys():
+        if 'endDate' not in params.keys():
+            error = 'Query contains startDate with no endDate.'
+            raise mlbgame.exceptions.ScheduleParamaterException(error)
+    if 'endDate' in params.keys():
+        if 'startDate' not in params.keys():
+            error = 'Query contains endDate with no startDate.'
+            raise mlbgame.exceptions.ScheduleParamaterException(error)
+    if 'opponentId' in params.keys():
+        if 'teamId' not in params.keys():
+            error = 'Query contains opponentId with no teamId.'
+            raise mlbgame.exceptions.ScheduleParamaterException(error)
     return request(8, 'schedule', params=params)
